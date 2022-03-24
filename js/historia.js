6354ce2a87ebd1bd69445d9c972de947ea0a3177
Henrique Marques Santos Silva <henriquemarquessantossilva@hotmail.com>
2022-03-24 19:28:18 -0300

    FirstSiteFrontEnd

---
+++ js/historia.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1, //quantos slides iram passar em uso
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  }); 
